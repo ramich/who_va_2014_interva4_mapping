de5c07248a69a2637075d0d2607eb3af26f54eb9
ramich <rajib.mitra@unibas.ch>
2015-07-23 16:41:54 +0200

    Cleaned unused columns in mapping files / Cleaned up script

--- WHO_INTERVA4_MAPPING.R
+++ WHO_INTERVA4_MAPPING.R
@@ -1,8 +1,8 @@
 ########### WHO VA 2014 -> InterVA4 file format mapping ################
 ## ----------------------------------------------------------------------
 ##
-## @description: 	Converts WHO VA 2014 submission to InterVA4 file format 
-##			ready to be coded in InterVA4.
+## @description: 	Converts 2014 WHO VA Tools submissions to InterVA4 file 
+##			format ready to be coded in InterVA4.
 ## @input:		mapping file who 2014 -> interva4 variables, and
 ##			who 2014 submission csv (exported from odk aggregate)
 ## @output:		csv for usage in InterVA4
@@ -10,6 +10,14 @@
 ## @date: 		07.2015
 ##
 ## ----------------------------------------------------------------------
+
+# load the required libraries:
+
+#load InterVA4 package
+library(InterVA4)
+
+#load foreach package
+library(foreach)
 
 cat("\nWHO VA Instrument 2014 -> InterVA4 Conversion\n\n")
 
@@ -19,19 +27,15 @@ rm(list=ls(all=TRUE))
 #Start time
 ptm <- proc.time()
 
-## Deine your file path variables here
+## Define your file path variables here###############################
 ######################################################################
 ######################################################################
-
 workingDir = "C:/dev/workspace_R/who_va_2014_interva4_mapping/data";
 mappingFileName = "mappings.csv"
 submissionFileName = "who.csv"
 outputFileName = "outputData.csv"
 
-#Check if directory and files exist
-if(!dir.exists(workingDir) || !file.exists(mappingFileName) || !file.exists(submissionFileName)){
-	stop("Directory or file not found.")
-}
+######################################################################
 
 # store the current directory
 initial.dir<-getwd()
@@ -40,14 +44,6 @@ print(paste("initial Working directory:" , initial.dir))
 print(paste("Set to working directory:" , workingDir))
 setwd(workingDir)
 print(paste("Set to working directory to:" , getwd()))
-
-# load the necessary libraries:
-
-#load InterVA4 package
-library(InterVA4)
-
-#load foreach package
-library(foreach)
 
 #load who submission file:
 who = read.csv(submissionFileName)
@@ -96,17 +92,14 @@ who_n = nrow(mapping)
 #who_n = 1 # Limit to first x entries for testing purposes
 counter = 1
 
-print(who_n)
 x <- foreach(i=1:who_n) %do%{	
 
 	##Assign who variable
-	who_var = as.character(mapping[i,5]) # Convert to class character from factors
-	expression = as.character(mapping[i,9])
+	#who_var = as.character(mapping[i,4]) # Convert to class character from factors
+	expression = as.character(mapping[i,5])
 	interva = as.character(mapping[i, 2])
 	id = mapping[i, 1]
 	question = mapping[i,3]
-
-	#print(interva)
 	
 	#Set Column names and also ID
 	if(i == 1){
@@ -116,37 +109,23 @@ x <- foreach(i=1:who_n) %do%{
 	colnames(outputData)[i+1] <- interva
 
 	if(class(interva) == "character" && nchar(interva) > 0){
+		#value = getValueInSubmissionForWHOId(who_var)
 
-		#print(who_var, max.levels=0)
-		value = getValueInSubmissionForWHOId(who_var)
-
-		if(!is.null(value)){
-if(1==2){
-			print(counter)
-			print(paste("Id:",id))
-			print(paste("Question:",question))
-			print(paste("Got back value:", value))
-			print(paste("Expression:", expression), max.levels=0)
-			print(paste(who_var,":", get(who_var)))
-			print(paste("InterVA4:", interva))
-}
-			##Evaluate expression and set InterVA4 variable accordingly
-			retVal = eval(parse(text=expression))
-			#print(paste("Eval:", retVal))
-			if(retVal == TRUE){
+		##Evaluate expression and set InterVA4 variable accordingly
+		retVal = eval(parse(text=expression))
+		if(retVal == TRUE){
+			printTrueOutput = FALSE
+			if(printTrueOutput){
 				print(counter)
 				print(paste("Id:",id))
 				print(paste("Question:",question))
-				print(paste("Got back value:", value))
+				#print(paste("Got back value:", value))
 				print(paste("Expression:", expression), max.levels=0)
-				print(paste(who_var,":", get(who_var)))
+				#print(paste(who_var,":", get(who_var)))
 				print(paste("InterVA4:", interva))
-				##Evaluate expression and set InterVA4 variable accordingly
-				outputData[i+1] = 'y'
 			}
-			
-		}else{
-			print("------------------------Could not find variable-------------------")
+			##Evaluate expression and set InterVA4 variable accordingly
+			outputData[i+1] = 'y'
 		}
 	}else{
 		print("Empty")
@@ -156,7 +135,7 @@ if(1==2){
 
 colnames(outputData) <- toupper(colnames(outputData)) #Change colnames to all uppercase
 
-print(outputData) #output InterVA4 data input structure
+#print(outputData) #output InterVA4 data input structure
 
 #Write output to file (comma separated for interva4 input, no quotation marks)
 write.csv(outputData,file=outputFileName, quote = FALSE, na="", row.names=FALSE)
@@ -167,7 +146,7 @@ va <- InterVA(outputData, HIV = "l", Malaria = "l", directory = "VA test",
 filename = "VA_result_wt_code", output = "extended", append = FALSE,
 replicate = TRUE, groupcode = TRUE, write = TRUE)
 
-str(va) # Output InterVA4 object structure
+#str(va) # Output InterVA4 object structure
 
 cat("\nInterVA4 Result:\n\n")
 print(paste("Most likely WHO 2012 VA cause category:", va$VA[[1]]$CAUSE1))
@@ -185,12 +164,11 @@ cat("\n\n\n")
 etm<-proc.time() - ptm # End time
 print(paste("Total run time:", etm[3], "s")) #print elapsed time
 
-# load the dataset
+# change back to the original directory
+setwd(initial.dir)
+
 # unload the libraries
 detach("package:InterVA4")
 detach("package:foreach")
-
-# change back to the original directory
-setwd(initial.dir)
 
 cat("\nWHO VA Instrument -> InterVA4 Mapping Done!\n\n")
